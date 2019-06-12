b9093f7d6cdf1ee4bf340fed5bf494b7b100f720
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-06-12 17:10:49 +0300

    Initial Commit

---
+++ FirstSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirstSwiftUI
+//
+//  Created by Stepan Vasiljeu on 6/12/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
